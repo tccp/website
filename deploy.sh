29c3600234613c7847b049caf581f1c4b3d8340c
Josh Rogan <jjjr1122@gmail.com>
2015-10-20 22:55:43 -0400

    Create Deploy Script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+timestamp=$(date +%Y%m%d%H%M)
+echo "Attemping to upload tcc/_site directory to unixs..."
+scp -r tcc/_site/ tcp@unixs.cssd.pitt.edu:public/deploys/build${timestamp}
+echo "Attemping to link the new build..."
+ssh tcp@unixs.cssd.pitt.edu "rm -f /afs/pitt.edu/home/t/c/tcp/public/html && ln -s /afs/pitt.edu/home/t/c/tcp/public/deploys/build${timestamp} /afs/pitt.edu/home/t/c/tcp/public/html"
+
