bac3fa8e0eb9b3f51c62c1c392d1d61b41fbe3c5
Josh Rogan <jjjr1122@gmail.com>
2015-10-20 22:57:22 -0400

    output build name

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 timestamp=$(date +%Y%m%d%H%M)
-echo "Attemping to upload tcc/_site directory to unixs..."
+echo "Attemping to upload tcc/_site directory to unixs at public/deploys/build${timestamp}..."
 scp -r tcc/_site/ tcp@unixs.cssd.pitt.edu:public/deploys/build${timestamp}
 echo "Attemping to link the new build..."
 ssh tcp@unixs.cssd.pitt.edu "rm -f /afs/pitt.edu/home/t/c/tcp/public/html && ln -s /afs/pitt.edu/home/t/c/tcp/public/deploys/build${timestamp} /afs/pitt.edu/home/t/c/tcp/public/html"
